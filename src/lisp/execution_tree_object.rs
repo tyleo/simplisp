eb1aa275f2766281b4a4ed139efa6adff0c4bf35
Tyler Wolf Leonhardt <tyler.wolf.leonhardt@gmail.com>
2016-06-01 20:15:25 -0700

    Add From impls to ExecutionTreeObject.

--- src/lisp/execution_tree_object.rs
+++ src/lisp/execution_tree_object.rs
@@ -47,3 +47,148 @@ impl ExecutionTreeObject {
         Ok(result)
     }
 }
+
+impl From<ExecutionTreeObject> for R<bool> {
+    fn from(object: ExecutionTreeObject) -> R<bool> {
+        match object {
+            ExecutionTreeObject::Bool(result) => Ok(result),
+            _ => panic!(),
+        }
+    }
+}
+
+impl From<ExecutionTreeObject> for R<char> {
+    fn from(object: ExecutionTreeObject) -> R<char> {
+        match object {
+            ExecutionTreeObject::Char(result) => Ok(result),
+            _ => panic!(),
+        }
+    }
+}
+
+impl From<ExecutionTreeObject> for R<f32> {
+    fn from(object: ExecutionTreeObject) -> R<f32> {
+        match object {
+            ExecutionTreeObject::F32(result) => Ok(result),
+            _ => panic!(),
+        }
+    }
+}
+
+impl From<ExecutionTreeObject> for R<f64> {
+    fn from(object: ExecutionTreeObject) -> R<f64> {
+        match object {
+            ExecutionTreeObject::F64(result) => Ok(result),
+            _ => panic!(),
+        }
+    }
+}
+
+impl From<ExecutionTreeObject> for R<i8> {
+    fn from(object: ExecutionTreeObject) -> R<i8> {
+        match object {
+            ExecutionTreeObject::I8(result) => Ok(result),
+            _ => panic!(),
+        }
+    }
+}
+
+impl From<ExecutionTreeObject> for R<i16> {
+    fn from(object: ExecutionTreeObject) -> R<i16> {
+        match object {
+            ExecutionTreeObject::I16(result) => Ok(result),
+            _ => panic!(),
+        }
+    }
+}
+
+impl From<ExecutionTreeObject> for R<i32> {
+    fn from(object: ExecutionTreeObject) -> R<i32> {
+        match object {
+            ExecutionTreeObject::I32(result) => Ok(result),
+            _ => panic!(),
+        }
+    }
+}
+
+impl From<ExecutionTreeObject> for R<i64> {
+    fn from(object: ExecutionTreeObject) -> R<i64> {
+        match object {
+            ExecutionTreeObject::I64(result) => Ok(result),
+            _ => panic!(),
+        }
+    }
+}
+
+impl From<ExecutionTreeObject> for R<isize> {
+    fn from(object: ExecutionTreeObject) -> R<isize> {
+        match object {
+            ExecutionTreeObject::ISize(result) => Ok(result),
+            _ => panic!(),
+        }
+    }
+}
+
+impl From<ExecutionTreeObject> for R<ExecutionTreeNode> {
+    fn from(object: ExecutionTreeObject) -> R<ExecutionTreeNode> {
+        match object {
+            ExecutionTreeObject::Node(result) => Ok(result),
+            _ => panic!(),
+        }
+    }
+}
+
+impl From<ExecutionTreeObject> for R<String> {
+    fn from(object: ExecutionTreeObject) -> R<String> {
+        match object {
+            ExecutionTreeObject::String(result) => Ok(result),
+            ExecutionTreeObject::Symbol(result) => Ok(result),
+            _ => panic!(),
+        }
+    }
+}
+
+impl From<ExecutionTreeObject> for R<u8> {
+    fn from(object: ExecutionTreeObject) -> R<u8> {
+        match object {
+            ExecutionTreeObject::U8(result) => Ok(result),
+            _ => panic!(),
+        }
+    }
+}
+
+impl From<ExecutionTreeObject> for R<u16> {
+    fn from(object: ExecutionTreeObject) -> R<u16> {
+        match object {
+            ExecutionTreeObject::U16(result) => Ok(result),
+            _ => panic!(),
+        }
+    }
+}
+
+impl From<ExecutionTreeObject> for R<u32> {
+    fn from(object: ExecutionTreeObject) -> R<u32> {
+        match object {
+            ExecutionTreeObject::U32(result) => Ok(result),
+            _ => panic!(),
+        }
+    }
+}
+
+impl From<ExecutionTreeObject> for R<u64> {
+    fn from(object: ExecutionTreeObject) -> R<u64> {
+        match object {
+            ExecutionTreeObject::U64(result) => Ok(result),
+            _ => panic!(),
+        }
+    }
+}
+
+impl From<ExecutionTreeObject> for R<usize> {
+    fn from(object: ExecutionTreeObject) -> R<usize> {
+        match object {
+            ExecutionTreeObject::USize(result) => Ok(result),
+            _ => panic!(),
+        }
+    }
+}
